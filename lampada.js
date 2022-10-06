52f81a79375755bd8126b5195db0febfa7e66974
Dorcas-chagas <109751669+Dorcas-chagas@users.noreply.github.com>
2022-10-06 11:55:38 -0300

    atualizando

--- lampada.js
+++ lampada.js
@@ -2,12 +2,20 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById ('lamp');
 
-function lampOn (){
-    lamp.src = './img/ybxlO.jpg';
+function isLampBroke () {
+    return lamp.src.indexOf ( 'quebrada' ) > -1;
 }
 
-function lampOff (){
-    lamp.src = './img/b983w.jpg';
+function lampOn (){
+    if (!isLampBroke () ) {
+        lamp.src = './img/ybxlO.jpg';
+    }
+}
+
+function lampOff () {
+    if (!isLampBroke () ) {
+        lamp.src = './img/b983w.jpg';
+    }
 }
 
 function lampBroke (){
@@ -16,7 +24,6 @@ function lampBroke (){
 
 turnOn.addEventListener ('click', lampOn);
 turnOff.addEventListener('click', lampOff);
-lampBroke.addEventListener('click', lampBroken)
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff);
-lampBroke.addEventListener('click', lampBroken);
+lamp.addEventListener('dblclick', lampBroke);
